19a177998b32db9c8eb33667d39011f24ae4eeb3
David Kučera <kucera.david2002@gmail.com>
2023-05-04 19:06:32 +0200

    finished daymonth, added doc comments

--- Uniza.Namedays/DayMonth.cs
+++ Uniza.Namedays/DayMonth.cs
@@ -1,22 +1,37 @@
 ﻿namespace Uniza.Namedays
 {
+    /// <summary>
+    /// Structure represents day and month
+    /// </summary>
     public record struct DayMonth
     {
         private int Day { get; init; }
         public int Month { get; init; }
 
+        /// <summary>
+        /// Non parametrized constructor sets the values to the actual date.
+        /// </summary>
         public DayMonth()
         {
             Day = DateTime.Now.Day;
             Month = DateTime.Now.Month;
         }
 
+        /// <summary>
+        /// Parametrized constructor sets the values to the values from parametres.
+        /// </summary>
+        /// <param name="day">Represents number of day.</param>
+        /// <param name="month">Represents number of month.</param>
         public DayMonth(int day, int month)
         {
             Day = day;
             Month = month;
         }
 
+        /// <summary>
+        /// Converts values to DateTime.
+        /// </summary>
+        /// <returns></returns>
         public DateTime ToDateTime()
         {
             return new DateTime(DateTime.Now.Year, Month, Day);
---
+++ Uniza.Namedays/Nameday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniza.Namedays
+{
+    public record struct Nameday
+    {
+        private string Name { get; init; }
+        private DayMonth DayMonth { get; init; }
+
+        public Nameday()
+        {
+            Name = "";
+            DayMonth = new DayMonth();
+        }
+
+        public Nameday(string name, DayMonth dayMonth)
+        {
+            Name = name;
+            DayMonth = dayMonth;
+        }
+    }
+}
