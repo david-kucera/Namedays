f788a2a696f85aa4292e2d8a1097d780f944493b
David Kučera <kucera.david2002@gmail.com>
2023-05-04 19:09:14 +0200

    finished docs to Nameday

--- Uniza.Namedays/Nameday.cs
+++ Uniza.Namedays/Nameday.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace Uniza.Namedays
 {
+    /// <summary>
+    /// Structure represents nameday celebration day.
+    /// </summary>
     public record struct Nameday
     {
         private string Name { get; init; }
         private DayMonth DayMonth { get; init; }
 
+        /// <summary>
+        /// Non parametrized constructor, sets Name to empty string and DayMonth to actual day.
+        /// </summary>
         public Nameday()
         {
-            Name = "";
+            Name = string.Empty;
             DayMonth = new DayMonth();
         }
 
+        /// <summary>
+        /// Parametrized constructor, sets values according to the parametres.
+        /// </summary>
+        /// <param name="name">name of celebrator.</param>
+        /// <param name="dayMonth">DayMonth of celebration.</param>
         public Nameday(string name, DayMonth dayMonth)
         {
             Name = name;
